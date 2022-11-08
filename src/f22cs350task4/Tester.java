09080d6523cbb7277d9b9f44cde4ea19ad2c740d
zacheryowens99 <zacheryowens99@gmail.com>
2022-11-08 10:23:36 -0800

    Changes to Tester.java

--- src/f22cs350task4/Tester.java
+++ src/f22cs350task4/Tester.java
@@ -9,46 +9,41 @@ public class Tester {
         System.out.println("ID: " + linear.getID());
         System.out.println("State Start: " + linear.getStateStart());
         System.out.println("State End: " + linear.getStateEnd());
-        System.out.println("State Step: " + linear.getStep() + "\n");
+        System.out.println("State Step: " + linear.getStep());
+        System.out.println("Is it dying? " + linear.isDying());
+        System.out.println("Is it dead? " + linear.isDead());
+        System.out.println("Current State: " + linear.getState()  + "\n" );
 
-        for(int i = 1; i <= 4; i++){
+        for(int i = 1; i <= 7; i++){
 
-           linear.updateState();
-            System.out.println("Update: " + i);
-            System.out.println("State Start: " + linear.getStateStart());
-            System.out.println("State End: " + linear.getStateEnd());
-            System.out.println("State Step: " + linear.getStep());
-            System.out.println("Current State: " + linear.getState());
-            System.out.println("Is it dying? " + linear.isDying());
-            System.out.println("Is it dead? " + linear.isDead() + "\n" );
-        }
+            if (i == 5){//change the value for i depending on where you want to terminate
+                System.out.println("Terminating\n");
+                linear.terminate();
 
-        System.out.println("Terminating");
-        linear.terminate();
-        System.out.println("Is it dying? " + linear.isDying() + "\n");
+                //System.out.println("Canceled\n");
+                //linear.cancel();
+            }
 
-        //System.out.println("Canceled");
-        //linear.cancel();
-
-        for(int i = 5; i <= 8; i++) {
             linear.updateState();
             System.out.println("Update: " + i);
             System.out.println("State Start: " + linear.getStateStart());
             System.out.println("State End: " + linear.getStateEnd());
             System.out.println("State Step: " + linear.getStep());
-            System.out.println("Current State: " + linear.getState());
             System.out.println("Is it dying? " + linear.isDying());
-            System.out.println("Is it dead? " + linear.isDead() + "\n" );
+            System.out.println("Is it dead? " + linear.isDead());
+            System.out.println("Current State: " + linear.getState()  + "\n" );
         }
+
 
         System.out.println("Final State: ");
         System.out.println("State Start: " + linear.getStateStart());
         System.out.println("State End: " + linear.getStateEnd());
         System.out.println("State Step: " + linear.getStep());
-        System.out.println("Current State: " + linear.getState());
         System.out.println("Is it dying? " + linear.isDying());
-        System.out.println("Is it dead? " + linear.isDead() + "\n" );
+        System.out.println("Is it dead? " + linear.isDead());
+        System.out.println("Current State: " + linear.getState() + "\n");
 
+//-----------------------------------------------------------------------------------------------------------------------
 
         System.out.println("Nonlinear\n");
         MyComponentNonlinear nonlinear = new MyComponentNonlinear("nonlinear13", -10, 30, 2.5, 1.5);
@@ -56,45 +51,40 @@ public class Tester {
         System.out.println("State Start: " + nonlinear.getStateStart());
         System.out.println("State End: " + nonlinear.getStateEnd());
         System.out.println("State Step: " + nonlinear.getStep());
-        System.out.println("Step Acceleration: " + nonlinear.getStepAcceleration()+ "\n");
+        System.out.println("Step Acceleration: " + nonlinear.getStepAcceleration());
+        System.out.println("Is it dying? " + nonlinear.isDying());
+        System.out.println("Is it dead? " + nonlinear.isDead());
+        System.out.println("Current State: " + nonlinear.getState() + "\n" );
 
         for(int i = 1; i <= 4; i++){
 
+            if (i == 3){
+                System.out.println("Terminating\n");
+                nonlinear.terminate();
+
+                //System.out.println("Canceled\n");
+                //linear.cancel();
+            }
+
             nonlinear.updateState();
             System.out.println("Update: " + i);
             System.out.println("State Start: " + nonlinear.getStateStart());
             System.out.println("State End: " + nonlinear.getStateEnd());
             System.out.println("State Step: " + nonlinear.getStep());
-            System.out.println("Current State: " + nonlinear.getState());
             System.out.println("Is it dying? " + nonlinear.isDying());
-            System.out.println("Is it dead? " + nonlinear.isDead() + "\n" );
+            System.out.println("Is it dead? " + nonlinear.isDead());
+            System.out.println("Current State: " + nonlinear.getState() + "\n" );
+
         }
 
-        System.out.println("Terminating");
-        nonlinear.terminate();
-        System.out.println("Is it dying? " + linear.isDying() + "\n");
-
-        //System.out.println("Canceled");
-        //linear.cancel();
-
-        for(int i = 5; i <= 6; i++) {
-            nonlinear.updateState();
-            System.out.println("Update: " + i);
-            System.out.println("State Start: " + nonlinear.getStateStart());
-            System.out.println("State End: " + nonlinear.getStateEnd());
-            System.out.println("State Step: " + nonlinear.getStep());
-            System.out.println("Current State: " + nonlinear.getState());
-            System.out.println("Is it dying? " + nonlinear.isDying());
-            System.out.println("Is it dead? " + nonlinear.isDead() + "\n" );
-        }
 
         System.out.println("Final State: ");
         System.out.println("State Start: " + nonlinear.getStateStart());
         System.out.println("State End: " + nonlinear.getStateEnd());
         System.out.println("State Step: " + nonlinear.getStep());
-        System.out.println("Current State: " + nonlinear.getState());
         System.out.println("Is it dying? " + nonlinear.isDying());
-        System.out.println("Is it dead? " + nonlinear.isDead() + "\n" );
+        System.out.println("Is it dead? " + nonlinear.isDead());
+        System.out.println("Current State: " + nonlinear.getState() + "\n" );
 
     }
 }
